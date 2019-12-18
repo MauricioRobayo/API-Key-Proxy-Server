cc91fceb25597c851a8b4802aa3e0659936de622
Mauricio Robayo <rfmajo@gmail.com>
2019-12-18 12:30:11 -0500

    refactor: use http-proxy-middleware

---
+++ proxies/weather-proxy.js
@@ -0,0 +1,34 @@
+const proxy = require('http-proxy-middleware')
+const querystring = require('querystring')
+
+// The route this proxy will match
+const context = '/weather'
+
+// The list of domains allowed to request to the proxy
+const allowedDomains = ['https://mauriciorobayo.github.io/weather-app']
+
+const filter = (pathname, req) => {
+  return (
+    pathname.match(`^${context}$`) &&
+    req.method === 'GET' &&
+    allowedDomains.includes(req.headers.origin)
+  )
+}
+
+const options = {
+  target: 'https://api.openweathermap.org',
+  changeOrigin: true,
+  pathRewrite(path, req) {
+    const queryparams = querystring.stringify({
+      ...req.query,
+      appid: process.env.WEATHER_API_KEY,
+    })
+    const newPath = path
+      .split('?')[0]
+      .replace(context, `/data/2.5/weather?${queryparams}`)
+    return newPath
+  },
+  logLevel: 'info', // change to 'debug' for more info
+}
+
+module.exports = proxy(filter, options)
