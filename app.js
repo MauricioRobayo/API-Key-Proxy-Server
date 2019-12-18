e84f69b50d127ea147a3df0f952286271ce08d99
Mauricio Robayo <rfmajo@gmail.com>
2019-12-18 12:30:11 -0500

    refactor: use http-proxy-middleware

--- app.js
+++ app.js
@@ -1,37 +1,12 @@
 const express = require('express')
-const cors = require('cors')
-const morgan = require('morgan')
-const fetch = require('node-fetch')
+const debug = require('debug')('express:server')
+const weatherProxy = require('./proxies/weather-proxy')
 
 require('dotenv').config()
 
 const app = express()
-app.use(morgan('tiny'))
 
-const allowedOrigins = ['https://mauriciorobayo.github.io']
-const corsOptions = {
-  origin(origin, callback) {
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  },
-}
-
-app.get('/weather', cors(corsOptions), (req, res) => {
-  const API_URL = 'https://api.openweathermap.org/'
-  const API_ENDPOINT = '/data/2.5/weather'
-  const url = new URL(API_ENDPOINT, API_URL)
-
-  url.search = new URLSearchParams({
-    ...req.query,
-    appid: process.env.WEATHER_API_KEY,
-  }).toString()
-  fetch(url.href)
-    .then(response => response.json())
-    .then(json => res.json(json))
-})
+app.use(weatherProxy)
 
 function notFound(req, res, next) {
   res.status(404)
@@ -51,4 +26,4 @@ app.use(notFound)
 app.use(errorHandler)
 
 const port = process.env.PORT || 5000
-app.listen(port, () => console.log(`Listening on port ${port}`))
+app.listen(port, () => debug(`Listening on port ${port}`))
