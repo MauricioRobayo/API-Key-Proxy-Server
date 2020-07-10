6a3b2179b10da799ccd50820815b30a62dc31ab6
Mauricio Robayo <rfmajo@gmail.com>
2020-07-10 11:34:03 -0500

    feat: allow all httpProxyMiddleware options

--- src/proxies.ts
+++ src/proxies.ts
@@ -10,47 +10,44 @@ import config from './config';
 
 const { allowedDomains: globalAllowedDomains = [], proxies } = config;
 
-const proxiesMiddlewares: RequestHandler[] = proxies.map(
-  ({
+const proxiesMiddlewares: RequestHandler[] = proxies.map((options) => {
+  const {
     route,
-    target,
     allowedDomains = [],
     allowedMethods = ['GET'],
     queryparams = {},
-    headers = {},
-    auth,
     changeOrigin = true,
-  }) => {
-    const filter: Filter = (pathname: string, req: Request) => {
-      if (typeof req.headers.origin === 'string') {
-        return (
-          pathname.startsWith(route) &&
-          allowedMethods.includes(req.method) &&
-          [...globalAllowedDomains, ...allowedDomains].includes(
-            req.headers.origin
-          )
-        );
-      }
-      return false;
-    };
-    const options: Options = {
-      target,
-      changeOrigin,
-      headers,
-      auth,
-      pathRewrite(path: string, req: Request) {
-        const qp = querystring.stringify({
-          ...req.query,
-          ...queryparams,
-        } as ParsedUrlQueryInput);
-        const newPath = `${path.split('?')[0].replace(route, '')}?${qp}`;
-        return newPath;
-      },
-      logLevel: process.env.NODE_ENV === 'development' ? 'debug' : 'info',
-    };
+    ...rest
+  } = options;
 
-    return createProxyMiddleware(filter, options);
-  }
-);
+  const filter: Filter = (pathname: string, req: Request) => {
+    if (typeof req.headers.origin === 'string') {
+      return (
+        pathname.startsWith(route) &&
+        allowedMethods.includes(req.method) &&
+        [...globalAllowedDomains, ...allowedDomains].includes(
+          req.headers.origin
+        )
+      );
+    }
+    return false;
+  };
+
+  const httpProxyMiddlewareOptions: Options = {
+    ...rest,
+    changeOrigin,
+    pathRewrite(path: string, req: Request) {
+      const qp = querystring.stringify({
+        ...req.query,
+        ...queryparams,
+      } as ParsedUrlQueryInput);
+      const newPath = `${path.split('?')[0].replace(route, '')}?${qp}`;
+      return newPath;
+    },
+    logLevel: process.env.NODE_ENV === 'development' ? 'debug' : 'info',
+  };
+
+  return createProxyMiddleware(filter, httpProxyMiddlewareOptions);
+});
 
 export default proxiesMiddlewares;
